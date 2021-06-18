ed9ffa6a0e041f4fdd9efd633b63f3b9b6a063e7
luankefei <luankefei@gmail.com>
2021-06-18 16:32:45 +0800

    docs(global): global declaration file

---
+++ src/interface/global.type.ts
@@ -0,0 +1,7 @@
+/* If your library has properties exposed on a global variable,
+ * place them here.
+ * You should also place types (interfaces and type alias) here.
+ */
+declare namespace _ {}
+
+export default _
