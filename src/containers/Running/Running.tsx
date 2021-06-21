ed72f46873fe6bb51691273af60c9c50f1f017cc
luankefei <luankefei@gmail.com>
2021-06-21 10:44:18 +0800

    feat(running): add page running

---
+++ src/containers/Running/Running.tsx
@@ -0,0 +1,17 @@
+import { useEffect } from 'react'
+import Head from 'next/head'
+
+const Running = () => {
+  useEffect(() => {})
+
+  return (
+    <div>
+      <Head>
+        <title>Sunken.me</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  )
+}
+
+export default Running
---
+++ src/containers/Running/running.style.ts
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled'
+
+const Container = styled.div`
+  border: 1px solid red;
+`
+
+export default {
+  Container
+}
