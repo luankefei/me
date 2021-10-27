815706ea0f8baef4541b03f7356fcb212cad7ac0
luankefei <luankefei@gmail.com>
2021-10-27 16:16:02 +0800

    backup: 首页暂时切换到running

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 // import Head from 'next/head'
-import Home from './home'
+// import Home from './home'
+import Running from './running'
 
-export default () => <Home />
+export default () => <Running />
